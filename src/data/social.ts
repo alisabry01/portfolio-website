0c8419aa5d8a2021910637169ab46cb079d0b8ce
ammarelbadry1 <ammarelbadry1@gmail.com>
2026-03-10 17:27:21 +0200

    fix: Update profession description in social info and correct resume link

--- src/data/social.ts
+++ src/data/social.ts
@@ -6,9 +6,9 @@ export const socialInfo = {
   title: "software engineer",
   country: "Dumyat, Egypt",
   description:
-    "Frontend by profession, full-stack by passion. Interested in web engineering. Building high-performance, scalable solutions.",
+    "Backend by profession, full-stack by passion. Interested in web engineering. Building high-performance, scalable solutions.",
   resume:
-    "https://docs.google.com/document/d/1ldqHQQedi_MnLRyHJcVgIbqXQyNxB2wqNLcbkqKqmlU/edit?usp=sharing",
+    "https://docs.google.com/document/d/14lQ7xtmMIjN9qboeD0f8fsRT9gRqqqTpCcvYhhFejBI/edit?usp=sharing",
   linkedin: "https://www.linkedin.com/in/ammarelbadry1",
   github: "https://github.com/ammarelbadry1",
   email: "ammarelbadry1@gmail.com",
